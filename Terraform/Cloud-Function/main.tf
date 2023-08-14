ef8d9a1deee36460538b2be8137a81eb0141ffba
Dilip Rathod <rathod.dt@gmail.com>
2023-08-14 12:40:45 +0530

    Cloud functions

--- Terraform/Cloud-Function/main.tf
+++ Terraform/Cloud-Function/main.tf
@@ -5,13 +5,23 @@ resource "google_storage_bucket_object" "GCS1" {
 }
 
 resource "google_cloudfunctions_function" "function" {
-  name        = "function-test"
-  description = "My function"
-  runtime     = "nodejs16"
+  name        = "cfun-tf"
+  description = "My function using terraform"
+  runtime     = "nodejs20"
 
   available_memory_mb   = 128
-  source_archive_bucket = google_storage_bucket.bucket.name
-  source_archive_object = google_storage_bucket_object.archive.name
+  source_archive_bucket = "terraform-bkt-backend-03"
+  source_archive_object = google_storage_bucket_object.GCS1.name
   trigger_http          = true
-  entry_point           = "helloGET"
+  entry_point           = "helloHttp-tf"
+}
+
+
+
+resource "google_cloudfunctions_function_iam_member" "member" {
+  project = google_cloudfunctions_function.function.project
+  region = google_cloudfunctions_function.function.region
+  cloud_function = google_cloudfunctions_function.function.name
+  role = "roles/cloudfunctions.invoker"
+  member = "allUsers"
 }
