2e2dcadcf86d4dd26dcfc005c3477501a2197e94
Dilip Rathod <rathod.dt@gmail.com>
2023-08-14 12:21:39 +0530

    Cloud run demo

--- Terraform/Cloud-Function/main.tf
+++ Terraform/Cloud-Function/main.tf
@@ -3,3 +3,15 @@ resource "google_storage_bucket_object" "GCS1" {
   name = "index.zip"
   source = "index.zip"
 }
+
+resource "google_cloudfunctions_function" "function" {
+  name        = "function-test"
+  description = "My function"
+  runtime     = "nodejs16"
+
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.archive.name
+  trigger_http          = true
+  entry_point           = "helloGET"
+}
