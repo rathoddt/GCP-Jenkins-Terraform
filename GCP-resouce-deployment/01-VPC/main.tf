5083fa5a17f2669ed5ddb6b4487454a28a8f675b
Dilip Rathod <rathod.dt@gmail.com>
2024-07-31 04:48:30 +0000

    VPC creation using terraform

--- GCP-resouce-deployment/01-VPC/main.tf
+++ GCP-resouce-deployment/01-VPC/main.tf
@@ -1,9 +1,18 @@
 provider "google" {
-  #credentials=file("terraform-key.json")
-  project = "{{YOUR GCP PROJECT}}"
-  region  = "us-central1"
-  zone    = "us-central1-c"
+  credentials=file("/home/vagrant/git-repos/GCP-Jenkins-Terraform/GCP-resouce-deployment/keys/tf-key.json")
+  project = "terraform-poc-01"
+  region  = "asia-south1"
+  zone    = "asia-south1-a"
 }
 resource "google_compute_network" "vpc_network" {
-  name                    = "terraform-network"
+  name       = "terraform-network"
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "terraform-bkt-poc-01"
+    prefix = "tf-state-files/vpc-01" #path
+    #credentials="$GOOGLE_APPLICATION_CREDENTIALS"
+    credentials = "/home/vagrant/git-repos/GCP-Jenkins-Terraform/GCP-resouce-deployment/keys/tf-key.json"
+  }
 }
