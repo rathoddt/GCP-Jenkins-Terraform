3d17c341547236985271925388ceefddb17b154d
Dilip Rathod <rathod.dt@gmail.com>
2024-07-31 08:01:22 +0000

    Setting up webserver uisng terraform along with vpc, subnet anf firwall rules

---
+++ GCP-resouce-deployment/03-language-basics/terraform-manifests/c2-provider.tf
@@ -0,0 +1,6 @@
+# Prtovider block
+
+provider "google" {
+  project = "terraform-poc-01"
+  region = "us-central1"
+}
---
+++ GCP-resouce-deployment/03-language-basics/terraform-manifests/c4-firewallrules.tf
@@ -0,0 +1,26 @@
+resource "google_compute_firewall" "fw_ssh" {
+  name    = "fwrule-allow-ssh"
+  network = google_compute_network.vpc_network.id
+  direction = "INGRESS"
+  priority = 1100
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]  
+  }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags = ["ssh-tag"]  
+}
+
+
+resource "google_compute_firewall" "fw_http" {
+  name    = "fwrule-allow-http80"
+  network = google_compute_network.vpc_network.id
+  direction = "INGRESS"
+  priority = 1100
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]  
+  }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags = ["webserver-tag"]  
+}
