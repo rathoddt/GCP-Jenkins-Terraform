0f1e49c9f5d994b0c21184e4158ae74924be4daa
Dilip Rathod <rathod.dt@gmail.com>
2024-08-04 06:33:19 +0000

    Terraform image template

---
+++ GCP-resouce-deployment/09-Instance-template-n-local-values/c2-variables.tf
@@ -0,0 +1,37 @@
+variable "gcp_project" {
+  description = "GCP priject name"
+  type        = string
+  default     = "terraform-poc-01"
+}
+
+variable "region" {
+  description = "Traget region"
+  type        = string
+  default     = "us-central1"
+}
+
+# variable "zone" {
+#   description = "Traget region"
+#   type        = string
+#   default     = "us-central1-a"
+# }
+
+variable "machine_type" {
+  description = "VM machine type"
+  type        = string
+  default     = "e2-small"
+}
+
+# Environment Variable
+variable "environment" {
+  description = "Environment Variable used as a prefix"
+  type = string
+  default = "dev"
+}
+
+# Business Division
+variable "business_divsion" {
+  description = "Business Division in the large organization this Infrastructure belongs"
+  type = string
+  default = "sap"
+}
